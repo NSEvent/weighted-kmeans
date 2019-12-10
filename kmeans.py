0527c32dd083fea9cb080497ed67904650a0bdb9
Kevin Tang <kvntng@gmail.com>
2019-12-10 18:15:28 -0500

    Added counting of number of clients per server, also altered file output--now maps each client to a server

--- kmeans.py
+++ kmeans.py
@@ -1,7 +1,33 @@
 import os
+import math
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+
+def find_euclidian_distance(xycoor1, xycoor2):
+	"""Return Euclidian distance between two (x, y) coordinates."""
+	d_sq = (xycoor2[0] - xycoor1[0])**2 + (xycoor2[1] - xycoor1[1])**2
+	return math.sqrt(d_sq)
+
+
+def find_total_distance(xlist, ylist, labels, centroids):
+	"""Return total distance between xy and centroid, prints centroid count."""
+	centroids_count = []
+	for _ in centroids:
+		centroids_count.append(0)
+
+	# import pdb; pdb.set_trace()
+	xylist = list(zip(xlist, ylist))
+
+	total_distance = 0
+	for xycoor, c_index in zip(xylist, labels):
+		centroid_coor = centroids[c_index]
+		centroids_count[c_index] += 1
+		total_distance += find_euclidian_distance(xycoor, centroid_coor)
+
+	print(centroids_count)
+	return total_distance
+
 
 MIN_CLUSTERS = 4
 MAX_CLUSTERS = 9
@@ -9,6 +35,9 @@ MAX_CLUSTERS = 9
 # Read in location data and weights
 cols = ['x', 'y', 'weight']
 data = pd.read_csv('location_data.csv', names=cols)
+
+xcoor0 = data.x.tolist()
+ycoor0 = data.y.tolist()
 
 xcoor = data.x.tolist()
 ycoor = data.y.tolist()
@@ -30,15 +59,29 @@ if not os.path.exists('results'):
 for num_clusters in range(MIN_CLUSTERS, MAX_CLUSTERS + 1):
 	kmeans = KMeans(n_clusters=num_clusters).fit(df)
 	centroids = kmeans.cluster_centers_
-	with open(f'results/N={num_clusters}-centroids.txt', 'w') as fout:
-		fout.write('CENTROID COORDINATES (x, y):\n')
-		fout.write(str(centroids))
-	
+
+	# Write centroid info
+	with open(f'results/N={num_clusters}-centroids.csv', 'w') as fout:
+		fout.write('CENTROID COORDINATES:\n')
+		fout.write('centroid_num, x, y\n')
+		for i, c in enumerate(centroids):
+			fout.write(f'{i}, {round(c[0], 3)}, {round(c[1], 3)}\n')
+
+	# Write client info
+	with open(f'results/N={num_clusters}-results.csv', 'w') as fout:
+		fout.write('CLIENT COORDINATES AND CENTROID NUMBER\n')
+		fout.write('x, y, centroid_no\n')
+		for x, y, n in zip(xcoor0, ycoor0, kmeans.labels_[0:len(xcoor0)-1]):
+			fout.write(f'{round(x, 3)}, {round(y, 3)}, {n}\n')
+
 	# Save clusters plot
 	plt.scatter(df['x'], df['y'], c=kmeans.labels_.astype(float), s=50, alpha=0.5)
-	plt.savefig(f'results/N={num_clusters}-grouped.png')
-	
+	# plt.savefig(f'results/N={num_clusters}-grouped.png')
+
 	# Save clusters with centroids plot
 	plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
 	plt.savefig(f'results/N={num_clusters}-grouped-with-center.png')
 	plt.close()
+
+	total_distance = find_total_distance(xcoor0, ycoor0, kmeans.labels_[0:len(xcoor0)-1], centroids)
+	print(total_distance)
