c6b60570763d65ce478f938d702f99882b318f02
Kevin Tang <kvntng@gmail.com>
2019-12-10 15:16:05 -0500

    kmeans.py now writes to files in the results director

--- kmeans.py
+++ kmeans.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
@@ -23,19 +24,21 @@ for i, wt in enumerate(weights):
 data = {'x': xcoor, 'y': ycoor}
 df = pd.DataFrame(data, columns=['x', 'y'])
 
+if not os.path.exists('results'):
+	os.mkdir('results')
 # Iterate through different num_clusters
 for num_clusters in range(MIN_CLUSTERS, MAX_CLUSTERS + 1):
 	kmeans = KMeans(n_clusters=num_clusters).fit(df)
 	centroids = kmeans.cluster_centers_
-	with open(f'N={num_clusters}-centroids.txt', 'w') as fout:
+	with open(f'results/N={num_clusters}-centroids.txt', 'w') as fout:
 		fout.write('CENTROID COORDINATES (x, y):\n')
 		fout.write(str(centroids))
 	
 	# Save clusters plot
 	plt.scatter(df['x'], df['y'], c=kmeans.labels_.astype(float), s=50, alpha=0.5)
-	plt.savefig(f'N={num_clusters}-grouped.png')
+	plt.savefig(f'results/N={num_clusters}-grouped.png')
 	
 	# Save clusters with centroids plot
 	plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
-	plt.savefig(f'N={num_clusters}-grouped-with-center.png')
+	plt.savefig(f'results/N={num_clusters}-grouped-with-center.png')
 	plt.close()
