291d340fce28c90f4b1a3f4f4b0d907ef066aa64
Kevin Tang <kvntng@gmail.com>
2019-12-10 14:55:04 -0500

    Added initial version of kmeans

---
+++ kmeans.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+MIN_CLUSTERS = 4
+MAX_CLUSTERS = 9
+
+# Read in location data and weights
+cols = ['x', 'y', 'weight']
+data = pd.read_csv('location_data.csv', names=cols)
+
+xcoor = data.x.tolist()
+ycoor = data.y.tolist()
+weights = data.weight.tolist()
+
+# Adjust x and y coors for weights
+# Weights are interpreted like frequency
+for i, wt in enumerate(weights):
+	for _ in range(1, wt):
+		xcoor.append(xcoor[i])
+		ycoor.append(ycoor[i])
+
+data = {'x': xcoor, 'y': ycoor}
+df = pd.DataFrame(data, columns=['x', 'y'])
+
+# Iterate through different num_clusters
+for num_clusters in range(MIN_CLUSTERS, MAX_CLUSTERS + 1):
+	kmeans = KMeans(n_clusters=num_clusters).fit(df)
+	centroids = kmeans.cluster_centers_
+	with open(f'N={num_clusters}-centroids.txt', 'w') as fout:
+		fout.write('CENTROID COORDINATES (x, y):\n')
+		fout.write(str(centroids))
+	
+	# Save clusters plot
+	plt.scatter(df['x'], df['y'], c=kmeans.labels_.astype(float), s=50, alpha=0.5)
+	plt.savefig(f'N={num_clusters}-grouped.png')
+	
+	# Save clusters with centroids plot
+	plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=50)
+	plt.savefig(f'N={num_clusters}-grouped-with-center.png')
+	plt.close()
